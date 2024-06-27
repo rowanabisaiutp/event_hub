fa48e8ee8d41fd86ab791dde57fd827d6120326a
YairHernandezPech <edilsonchelitohernandez011@gmail.com>
2024-06-26 19:25:08 -0600

    Profile xd

---
+++ lib/Profile/ProfileHome.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+class ProfileHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipPath(
+                  clipper: NavbarClipper(),
+                  child: Container(
+                    height: 265.0,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                           Color(0xFFD36AE4),
+                          Color(0x42E894BC)
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ),
+                    ),
+                  ),
+                ),
+                const Positioned(
+                  top: 100.0,
+                  left: 0,
+                  right: 0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 1),
+                      CircleAvatar(
+                        radius: 80.0,
+                        backgroundImage: NetworkImage(
+                            'https://cdn-icons-png.freepik.com/512/3135/3135715.png'), // Cambia esto por la ruta de tu imagen
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10.0),
+                    const Text(
+                      'Eider Pool',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      'John@gmail.com',
+                      style: TextStyle(
+                        fontSize: 23.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 214, 113, 229),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 35, vertical: 10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ), // Color del botón
+                      ),
+                      child: const Text(
+                        'Editar Perfil',
+                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      ),
+                    ),
+                    const SizedBox(height: 42.0),
+                    const Text(
+                      'Numero de telefono',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    const Text(
+                      '+52(999)929737',
+                      style: TextStyle(
+                        fontSize: 23.0,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    const Text(
+                      'Contraseña',
+                      style: TextStyle(
+                        fontSize: 23.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    const Text(
+                      '****************',
+                      style: TextStyle(
+                        fontSize: 23.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 90.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 25.0,
+                          backgroundColor: Colors.blue.shade100,
+                        ),
+                        const SizedBox(width: 10.0),
+                        CircleAvatar(
+                          radius: 25.0,
+                          backgroundColor: Colors.pink.shade100,
+                        ),
+                        const SizedBox(width: 10.0),
+                        CircleAvatar(
+                          radius: 25.0,
+                          backgroundColor: Colors.purple.shade100,
+                        ),
+                        const SizedBox(width: 10.0),
+                        const CircleAvatar(
+                          radius: 25.0,
+                          backgroundColor: Colors.purple,
+                          child: Icon(
+                            Icons.check,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 83.0),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 214, 113, 229),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 90, vertical: 8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ), // Color del botón
+                      ),
+                      child: const Text('Cerrar sesión', style: TextStyle(color: Colors.white, fontSize: 25.0),),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NavbarClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 100);
+    var firstControlPoint = Offset(size.width / 2, size.height);
+    var firstEndPoint = Offset(size.width, size.height - 100);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ThemeNotifier with ChangeNotifier {
+  ThemeData _currentTheme;
+  
+  ThemeNotifier(this._currentTheme);
+
+  ThemeData get currentTheme => _currentTheme;
+
+  void setTheme(ThemeData theme) {
+    _currentTheme = theme;
+    notifyListeners();
+  }
+}
+
+
+final ThemeData theme1 = ThemeData(
+  colorScheme: ColorScheme.fromSwatch().copyWith(
+    primary: const Color(0xFFD36AE4),
+    secondary: const Color(0x42E894BC),
+    tertiary: const Color.fromARGB(255, 214, 113, 229) //Color para los botones
+  ),
+);
+final ThemeData theme2 = ThemeData(
+  colorScheme: ColorScheme.fromSwatch().copyWith(
+    primary: const  Color.fromARGB(255, 228, 106, 204),
+    secondary: const Color.fromARGB(66, 232, 148, 218),
+    tertiary: const Color.fromARGB(255, 229, 113, 208)
+  ),
+);
+final ThemeData theme3 = ThemeData(
+  colorScheme: ColorScheme.fromSwatch().copyWith(
+    primary: const Color.fromARGB(255, 212, 106, 228),
+    secondary: const Color.fromARGB(66, 200, 148, 232),
+    tertiary: const Color.fromARGB(255, 210, 113, 229)
+  ),
+);
+final ThemeData theme4 = ThemeData(
+  colorScheme: ColorScheme.fromSwatch().copyWith(
+    primary: const Color.fromARGB(255, 106, 157, 228),
+    secondary: const Color.fromARGB(66, 148, 229, 232),
+    tertiary: const Color.fromARGB(255, 113, 184, 229)
+  ),
+);
