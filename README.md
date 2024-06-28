490385933f9a3a73cf8d8d4b92de9c91f6829706
Rowan Abisai <a.rowan.ojeda@utponiente.edu.mx>
2024-06-27 19:13:24 -0600

    Nota eider xd

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 En este proyecto se realizara la app Digital Event Hub
 para los eventos para la presentacion de proyectos en flutter
+
+#NOTA
+Modificar la linea 71 del archivo
+lib/sesion/login/login.dart
+
+cambia la funcion GoogleMapScreen() por el nombre
+de tu funcion del home que realizaras.
+
+builder: (context) => GoogleMapScreen(),
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ThemeNotifier with ChangeNotifier {
   ThemeData _currentTheme;
-  
+
   ThemeNotifier(this._currentTheme);
 
   ThemeData get currentTheme => _currentTheme;
@@ -13,32 +13,29 @@ class ThemeNotifier with ChangeNotifier {
   }
 }
 
-
 final ThemeData theme1 = ThemeData(
   colorScheme: ColorScheme.fromSwatch().copyWith(
-    primary: const Color(0xFFD36AE4),
-    secondary: const Color(0x42E894BC),
-    tertiary: const Color.fromARGB(255, 214, 113, 229) //Color para los botones
-  ),
+      primary: const Color(0xFFD36AE4),
+      secondary: const Color(0x42E894BC),
+      tertiary:
+          const Color.fromARGB(255, 214, 113, 229) //Color para los botones
+      ),
 );
 final ThemeData theme2 = ThemeData(
   colorScheme: ColorScheme.fromSwatch().copyWith(
-    primary: const  Color.fromARGB(255, 228, 106, 204),
-    secondary: const Color.fromARGB(66, 232, 148, 218),
-    tertiary: const Color.fromARGB(255, 229, 113, 208)
-  ),
+      primary: const Color.fromARGB(255, 228, 106, 204),
+      secondary: const Color.fromARGB(66, 232, 148, 218),
+      tertiary: const Color.fromARGB(255, 229, 113, 208)),
 );
 final ThemeData theme3 = ThemeData(
   colorScheme: ColorScheme.fromSwatch().copyWith(
-    primary: const Color.fromARGB(255, 212, 106, 228),
-    secondary: const Color.fromARGB(66, 200, 148, 232),
-    tertiary: const Color.fromARGB(255, 210, 113, 229)
-  ),
+      primary: const Color.fromARGB(255, 212, 106, 228),
+      secondary: const Color.fromARGB(66, 200, 148, 232),
+      tertiary: const Color.fromARGB(255, 210, 113, 229)),
 );
 final ThemeData theme4 = ThemeData(
   colorScheme: ColorScheme.fromSwatch().copyWith(
-    primary: const Color.fromARGB(255, 106, 157, 228),
-    secondary: const Color.fromARGB(66, 148, 229, 232),
-    tertiary: const Color.fromARGB(255, 113, 184, 229)
-  ),
+      primary: const Color.fromARGB(255, 106, 157, 228),
+      secondary: const Color.fromARGB(66, 148, 229, 232),
+      tertiary: const Color.fromARGB(255, 113, 184, 229)),
 );
